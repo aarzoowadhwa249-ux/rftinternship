349e1c8e723533ff2b8844caf308ef42789443c6
aarzoowadhwa249-ux <aarzoowadhwa249@gmail.com>
2026-05-04 20:26:57 +0530

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "Hello World";
+        String reversed = "";
+
+        // Loop through the string from end to start
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        System.out.println("Original String: " + original);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
